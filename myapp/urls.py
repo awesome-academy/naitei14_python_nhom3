e32fc3b585a6dd703aa0846e17f0e31f4189a86c
trangnh <hongtrangqn834@gmail.com>
2025-12-09 13:22:51 +0700

    [FEATURE][94868]: Implement user profileview, update, password change
    f
    
    [FEATURE][94865]: fix ModelForm and toast

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
+from .views import CustomPasswordChangeView
 
 app_name = 'myapp'
 
@@ -11,8 +13,9 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('profile/', views.render_profile_page, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('profile/update/', views.update_profile, name='update_profile'),
+    path("password-change/", CustomPasswordChangeView.as_view(), name="password_change"),
     
     # Email verification & Password reset
     path('activate/<str:token>/', views.activate_account, name='activate_account'),
